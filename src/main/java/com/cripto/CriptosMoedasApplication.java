4d1f7d87ffc4192f10af45e6f9529ede72853ebf
nathanfrc <nathan.frc@hotmail.com>
2021-11-03 02:08:02 -0300

    create project

---
+++ src/main/java/com/cripto/CriptosMoedasApplication.java
@@ -0,0 +1,13 @@
+package com.cripto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptosMoedasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptosMoedasApplication.class, args);
+	}
+
+}
